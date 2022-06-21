35dca8e7949c69c32029d593f3f9587dbd36fd8a
Dylan Rafferty <draff1800@gmail.com>
2022-06-21 22:59:31 +0100

    Added custom vscode Tasks. Completed design for first Workouts wireframe.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(const MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
-  static const String _title = 'BodybuildingPal';
- 
+
+  static const String _title = 'Workouts';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,42 +14,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
-      ),
+      home: const MyStatefulWidget(),
     );
   }
 }
- 
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
- 
+
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
- 
+
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
- 
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BodybuildingPal'),
+      ),
+      body: 
+        Column(
           children: <Widget>[
             Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Workouts',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.fromLTRB(20, 15, 0, 0),
+              child: const Text('Workouts', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35))
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.fromLTRB(0, 275, 0, 275),
+              child: const Text('There\'s nothing here... Yet!', style: TextStyle(color: Colors.grey))
+            ),
           ],
-        ));
+        ),
+      floatingActionButton: 
+        FloatingActionButton(
+          onPressed: () => {},
+          tooltip: 'Add Workout',
+          child: const Icon(Icons.add),
+        ),
+    );
   }
 }
