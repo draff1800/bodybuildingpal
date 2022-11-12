ed03607daa7a87dd3a5d060d62a62ab4c1c43e13
Dylan Rafferty <draff1800@gmail.com>
2022-11-12 14:25:10 +0000

    Improved styling of Profile screen. Radios and Fields now behaving as expected.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+enum Gender { male, female }
+
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -13,7 +15,10 @@ class Profile extends StatefulWidget {
 
 class ProfileState extends State<Profile> {
   final _formKey = GlobalKey<FormState>();
-  TextEditingController dobInput = TextEditingController();
+  Gender? _genderInput;
+  final TextEditingController _dobInput = TextEditingController();
+  final TextEditingController _heightInput = TextEditingController();
+
 
   @override
   void initState() {
@@ -34,88 +39,128 @@ class ProfileState extends State<Profile> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text(
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  child: Text(
                   'Attributes',
-                  style: TextStyle(
-                      color: Colors.lightBlue,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold
+                    ),
+                  )
                 ),
-                Row(children: [
-                  const Icon(Icons.transgender, size: 30),
-                  const Text('Gender',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Radio(
-                      value: "male",
-                      groupValue: "gender",
-                      onChanged: (value) => {}),
-                  const Text('Male', style: TextStyle(fontSize: 15)),
-                  Radio(
-                      value: "female",
-                      groupValue: "gender",
-                      onChanged: (value) => {}),
-                  const Text('Female', style: TextStyle(fontSize: 15)),
-                ]),
-                Row(children: [
-                  const Icon(Icons.calendar_month, size: 30),
-                  const Text('DOB',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  Container(
-                      constraints: const BoxConstraints.expand(
-                        height: 20,
-                        width: 200,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Row(children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: Icon(Icons.transgender, size: 40)
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: Text('Gender',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                      )
+                    ),
+                    Radio(
+                        value: Gender.male,
+                        groupValue: _genderInput,
+                        onChanged: (Gender? value) {
+                          setState(() {
+                            _genderInput = value;
+                          });
+                        }
+                    ),
+                    const Text('Male', style: TextStyle(fontSize: 15)),
+                    Radio(
+                        value: Gender.female,
+                        groupValue: _genderInput,
+                        onChanged: (Gender? value) {
+                          setState(() {
+                            _genderInput = value;
+                          });
+                        }
+                    ),
+                    const Text('Female', style: TextStyle(fontSize: 15)),
+                  ]),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: Row(children: [
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                        child: Icon(Icons.calendar_month, size: 40)
                       ),
-                      child: TextFormField(
-                          controller: dobInput,
-                          readOnly: true,
-                          onTap: () async {
-                            DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(DateTime.now().year - 120),
-                                lastDate: DateTime.now());
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 60, 0),
+                        child: Text('DOB',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                        )
+                      ),
+                      Container(
+                          constraints: const BoxConstraints.expand(
+                            height: 20,
+                            width: 170,
+                          ),
+                          child: TextFormField(
+                              controller: _dobInput,
+                              readOnly: true,
+                              onTap: () async {
+                                DateTime? pickedDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(DateTime.now().year - 120),
+                                    lastDate: DateTime.now());
 
-                            if (pickedDate != null) {
-                              String formattedDate =
-                                  DateFormat('yyyy-MM-dd').format(pickedDate);
-                              setState(() {
-                                dobInput.text = formattedDate;
-                              });
-                            } else {}
-                          }))
-                ]),
-                Row(children: [
-                  const RotatedBox(
-                    quarterTurns: 1,
-                    child: Icon(Icons.straighten, size: 30),
-                  ),
-                  const Text('Height',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                  ),
-                  Container(
-                      constraints: const BoxConstraints.expand(
-                        height: 20,
-                        width: 200,
-                      ),
-                      child: TextFormField()
-                  ),
-                  const Text('cm', style: TextStyle(fontSize: 15))
-                ]),
+                                if (pickedDate != null) {
+                                  String formattedDate =
+                                      DateFormat('yyyy-MM-dd').format(pickedDate);
+                                  setState(() {
+                                    _dobInput.text = formattedDate;
+                                  });
+                                } else {}
+                              }
+                          )
+                    )
+                    ])
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: Row(children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: Icon(Icons.straighten, size: 40),
+                      )
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 39, 0),
+                      child: Text('Height',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                      )
+                    ),
+                    Container(
+                        constraints: const BoxConstraints.expand(
+                          height: 20,
+                          width: 60,
+                        ),
+                        child: TextFormField(controller: _heightInput)
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: Text('cm', style: TextStyle(fontSize: 15))
+                    )
+                  ]),
+                ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8),
                   child: ElevatedButton(
-                      onPressed: () {
-                        // Validate returns true if the form is valid, or false otherwise.
-                        if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Processing Data')),
-                          );
-                        }
-                      },
+                      onPressed: null,
                       style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50)),
+                          minimumSize: const Size.fromHeight(50)
+                      ),
                       child: const Text('SAVE')),
                 ),
               ],
