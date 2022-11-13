e5c93f8949f35a57648237a832968734e9941620
Dylan Rafferty <draff1800@gmail.com>
2022-11-13 13:27:10 +0000

    Created app_colours constants file to centralise colour palette

---
+++ lib/constants/app_colours.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class AppColours {
+  AppColours._();
+  static const Color white = Color.fromARGB(255, 250, 250, 250);
+  static const Color grey = Color.fromARGB(255, 240, 240, 240);
+  static const Color blue = Color.fromARGB(255, 3, 169, 244);
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
+}
