a0fddf66e5ec6d5e7fb2862b8c77472820d47501
Dylan Rafferty <draff1800@gmail.com>
2022-11-15 22:53:20 +0000

    Removed form widget from Profile.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -13,7 +13,6 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  final _formKey = GlobalKey<FormState>();
   Gender? _genderInput;
   final TextEditingController _dobInput = TextEditingController();
   final TextEditingController _heightInput = TextEditingController();
@@ -40,141 +39,138 @@ class _ProfileState extends State<Profile> {
       ),
       body: Align(
         alignment: Alignment.topCenter,
-        child: Form(
-          key: _formKey,
-          child: Container(
-            color: AppColours.white,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
-                  child: Text(
-                  'Attributes',
-                    style: TextStyle(
-                        color: AppColours.blue,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold
-                    ),
-                  )
-                ),
-                Padding(
+        child: Container(
+          color: AppColours.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
+                child: Text(
+                'Attributes',
+                  style: TextStyle(
+                      color: AppColours.blue,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                  ),
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    child: Icon(Icons.transgender, size: 40)
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    child: Text('Gender',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                    )
+                  ),
+                  Radio(
+                      value: Gender.male,
+                      groupValue: _genderInput,
+                      onChanged: (Gender? value) {
+                        setState(() {
+                          _genderInput = value;
+                        });
+                      }
+                  ),
+                  const Text('Male', style: TextStyle(fontSize: 15)),
+                  Radio(
+                      value: Gender.female,
+                      groupValue: _genderInput,
+                      onChanged: (Gender? value) {
+                        setState(() {
+                          _genderInput = value;
+                        });
+                      }
+                  ),
+                  const Text('Female', style: TextStyle(fontSize: 15)),
+                ]),
+              ),
+              Padding(
                   padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
                   child: Row(children: [
                     const Padding(
                       padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                      child: Icon(Icons.transgender, size: 40)
+                      child: Icon(Icons.calendar_month, size: 40)
                     ),
                     const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                      child: Text('Gender',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                      )
-                    ),
-                    Radio(
-                        value: Gender.male,
-                        groupValue: _genderInput,
-                        onChanged: (Gender? value) {
-                          setState(() {
-                            _genderInput = value;
-                          });
-                        }
-                    ),
-                    const Text('Male', style: TextStyle(fontSize: 15)),
-                    Radio(
-                        value: Gender.female,
-                        groupValue: _genderInput,
-                        onChanged: (Gender? value) {
-                          setState(() {
-                            _genderInput = value;
-                          });
-                        }
-                    ),
-                    const Text('Female', style: TextStyle(fontSize: 15)),
-                  ]),
-                ),
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                    child: Row(children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                        child: Icon(Icons.calendar_month, size: 40)
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 60, 0),
-                        child: Text('DOB',
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                        )
-                      ),
-                      Container(
-                          constraints: const BoxConstraints.expand(
-                            height: 20,
-                            width: 170,
-                          ),
-                          child: TextFormField(
-                              controller: _dobInput,
-                              readOnly: true,
-                              onTap: () async {
-                                DateTime? pickedDate = await showDatePicker(
-                                    context: context,
-                                    initialDate: DateTime.now(),
-                                    firstDate: DateTime(DateTime.now().year - 120),
-                                    lastDate: DateTime.now());
-
-                                if (pickedDate != null) {
-                                  String formattedDate =
-                                      DateFormat('dd-MM-yyy').format(pickedDate);
-                                  setState(() {
-                                    _dobInput.text = formattedDate;
-                                  });
-                                }
-                              }
-                          )
-                      )
-                    ])
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(children: [
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                      child: RotatedBox(
-                        quarterTurns: 1,
-                        child: Icon(Icons.straighten, size: 40),
-                      )
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 39, 0),
-                      child: Text('Height',
+                      padding: EdgeInsets.fromLTRB(0, 0, 60, 0),
+                      child: Text('DOB',
                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
                       )
                     ),
                     Container(
                         constraints: const BoxConstraints.expand(
                           height: 20,
-                          width: 60,
+                          width: 170,
                         ),
-                        child: TextFormField(controller: _heightInput)
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                      child: Text('cm', style: TextStyle(fontSize: 15))
+                        child: TextField(
+                            controller: _dobInput,
+                            readOnly: true,
+                            onTap: () async {
+                              DateTime? pickedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(DateTime.now().year - 120),
+                                  lastDate: DateTime.now());
+
+                              if (pickedDate != null) {
+                                String formattedDate =
+                                    DateFormat('dd-MM-yyy').format(pickedDate);
+                                setState(() {
+                                  _dobInput.text = formattedDate;
+                                });
+                              }
+                            }
+                        )
                     )
-                  ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 15, 10, 40),
-                  child: ElevatedButton(
-                      onPressed: null,
-                      style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50)
+                  ])
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    child: RotatedBox(
+                      quarterTurns: 1,
+                      child: Icon(Icons.straighten, size: 40),
+                    )
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 39, 0),
+                    child: Text('Height',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                    )
+                  ),
+                  Container(
+                      constraints: const BoxConstraints.expand(
+                        height: 20,
+                        width: 60,
                       ),
-                      child: const Text('SAVE')),
-                ),
-              ],
-            ),
-          ) 
+                      child: TextField(controller: _heightInput)
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    child: Text('cm', style: TextStyle(fontSize: 15))
+                  )
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 10, 40),
+                child: ElevatedButton(
+                    onPressed: null,
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50)
+                    ),
+                    child: const Text('SAVE')),
+              ),
+            ],
+          )
         )
       ),
     );
