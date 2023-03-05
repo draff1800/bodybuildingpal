8a6858e1d2dff9eff3566208fae946b5e5560743
Dylan Rafferty <draff1800@gmail.com>
2023-03-05 18:07:15 +0000

    Added basic calendar to Logbook screen

--- lib/screens/home/logbook.dart
+++ lib/screens/home/logbook.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class LogBook extends StatefulWidget {
   const LogBook({super.key});
@@ -10,6 +11,10 @@ class LogBook extends StatefulWidget {
 class _LogBookState extends State<LogBook> {
   @override
   Widget build(BuildContext context) {
-    return const Text("logbook");
+    return TableCalendar(
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: DateTime.now()
+    );
   }
 }
