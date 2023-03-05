db2a6abf68c1cfe3052de15a13949bed92240955
Dylan Rafferty <draff1800@gmail.com>
2023-03-05 19:06:58 +0000

    Calendar header design progress

--- lib/screens/home/logbook.dart
+++ lib/screens/home/logbook.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class LogBook extends StatefulWidget {
@@ -9,12 +10,22 @@ class LogBook extends StatefulWidget {
 }
 
 class _LogBookState extends State<LogBook> {
+  final currentDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
-      firstDay: DateTime.utc(2010, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: DateTime.now()
+      firstDay: DateTime.utc(2000, 01, 01),
+      lastDay: DateTime(currentDate.year + 1, currentDate.month, currentDate.day),
+      focusedDay: currentDate,
+      headerStyle: const HeaderStyle(formatButtonVisible: false),
+      calendarBuilders: CalendarBuilders(
+        headerTitleBuilder: (context, day) {
+          return Text( "${DateFormat('MMMM').format(day)} ${day.year}",
+              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center);
+        },
+      ),
     );
   }
 }
