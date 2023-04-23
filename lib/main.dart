8ee7f2bb8baf1891cbe7a62f4457cc31619f8ce2
Dylan Rafferty <draff1800@gmail.com>
2023-04-23 16:13:50 -0400

    Logbook is Landing Page if Profile form is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,28 @@
 import 'package:bodybuildingpal/screens/profile.dart';
+import 'package:bodybuildingpal/screens/home/home_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import './constants/app_colours.dart';
 
-void main() => runApp(const BodybuildingPal());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool existingUser = (prefs.getString('gender') != null &&
+      prefs.getString('gender') != '' &&
+      prefs.getString('dob') != null &&
+      prefs.getString('dob') != '' &&
+      prefs.getString('height') != null &&
+      prefs.getString('height') != '');
 
-class BodybuildingPal extends StatelessWidget {
-  const BodybuildingPal({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(MaterialApp(
       title: "BodybuildingPal",
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        appBarTheme: const AppBarTheme(
-          foregroundColor: AppColours.black,
-          backgroundColor: AppColours.white,
-          centerTitle: true,
-          elevation: 0
-        ),
-        scaffoldBackgroundColor: AppColours.grey1
-      ),
-      home: const Profile()
-    );
-  }
+          primarySwatch: Colors.lightBlue,
+          appBarTheme: const AppBarTheme(
+              foregroundColor: AppColours.black,
+              backgroundColor: AppColours.white,
+              centerTitle: true,
+              elevation: 0),
+          scaffoldBackgroundColor: AppColours.grey1),
+      home: existingUser ? const HomeNavigation() : const Profile()));
 }
